07f7fe78ea834865e7b7180e185ce2b91449e66d
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-06-09 12:17:14 +0400

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -5,10 +5,10 @@ namespace Carnamespace
 	{
 		public string Manufacturer { get; set; }
 		public string Model { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
+        	public double Price { get; set; }
+        	public int Quantity { get; set; }
 
-        public Car(string manufacturer,string model,double price, int quantity)
+        	public Car(string manufacturer,string model,double price, int quantity)
 		{
 			Manufacturer = manufacturer;
 			Model = model;
@@ -16,29 +16,9 @@ namespace Carnamespace
 			Quantity = quantity;
 		}
 
-		public void Add(int amount)
-		{
-			Quantity += amount;
-		}
-
-		public bool Remove(int amount)
-		{
-			if (Quantity >= amount)
-			{
-				Quantity -= amount;
-				return true;
-			}
-			else
-			{
-				Console.WriteLine("Not Enough Cars");
-				return false;
-			}
-		}
-
-        public override string ToString()
-        {
+        	public override string ToString()
+        	{
 			return $"Amount - {Quantity}, {Manufacturer} {Model}: Price is: {Price}$";
         }
     }
 }
-
